b120af86d2fd75a4dedb8df647f8f80dbe661f68
hcayless <hcayless@71858626-4ce2-415c-9a67-6d0363ee0835>
2010-07-16 13:58:09 +0000

    Updates to images RDF and image linking for insert-reciprocals.
    git-svn-id: http://idp.atlantides.org/svn/idp/idp.mapping/trunk@3349 71858626-4ce2-415c-9a67-6d0363ee0835

--- bin/info/papyri/insert-reciprocals.clj
+++ bin/info/papyri/insert-reciprocals.clj
@@ -21,12 +21,23 @@
       construct{?s dc:relation ?o} 
       from <rmi://localhost/papyri.info#pi> 
       where { ?o dc:relation ?s}")
+  (def images "prefix dc: <http://purl.org/dc/terms/>
+      construct { ?r1 <http://purl.org/dc/terms/relation> ?r2 }
+      from <rmi://localhost/papyri.info#pi>
+      where {
+      ?c dc:isPartOf <http://papyri.info/apis> .
+      ?i dc:isPartOf ?c .
+      ?i dc:relation ?r1 .
+      ?i dc:relation ?r2 .
+      FILTER ( regex(str(?r1), \"^http://papyri.info/ddbdp\") || regex(str(?r1), \"^http://papyri.info/hgv\")) 
+      FILTER  regex(str(?r2), \"^http://papyri.info/images\")}")
   (let [factory (ConnectionFactory.)
         conn (.newConnection factory server)
         interpreter (SparqlInterpreter.)]
       (.execute conn (CreateGraph. graph))
       (.execute (Insertion. graph, (.parseQuery interpreter hasPart)) conn)
       (.execute (Insertion. graph, (.parseQuery interpreter relation)) conn)
+      (.execute (Insertion. graph, (.parseQuery interpreter images)) conn)
       (.close conn)))
       
 (-main (rest *command-line-args*))
